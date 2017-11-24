3f9c3c914d1d72338b7008bd4a28c157779fabf4
DuyQK <duyqk@runsystem.net>
2017-11-24 09:39:08 +0700

    init app

---
+++ Multiscreen-Xamarin/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace Multiscreen-Xamarin
+{
+	public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new Multiscreen_XamarinPage();
+    }
+
+    protected override void OnStart()
+    {
+        // Handle when your app starts
+    }
+
+    protected override void OnSleep()
+    {
+        // Handle when your app sleeps
+    }
+
+    protected override void OnResume()
+    {
+        // Handle when your app resumes
+    }
+}
+}
---
+++ Multiscreen-Xamarin/Multiscreen-XamarinPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Multiscreen-Xamarin
+{
+	public partial class Multiscreen_XamarinPage : ContentPage
+{
+    public Multiscreen_XamarinPage()
+    {
+        InitializeComponent();
+    }
+}
+}
